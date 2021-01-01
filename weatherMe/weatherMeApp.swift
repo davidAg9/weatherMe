754d75b306841424bae2814b18cec195c13fc1ca
davidAg9 <shane.qoubby@gmail.com>
2021-01-01 16:04:13 +0000

    Initial Commit

---
+++ weatherMe/weatherMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherMeApp.swift
+//  weatherMe
+//
+//  Created by Dave Agyakwa on 01/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct weatherMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
